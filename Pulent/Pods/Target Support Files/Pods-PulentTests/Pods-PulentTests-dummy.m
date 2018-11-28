074ec2764faf1b0c1aa79afc0d9ca489f11885f0
Matías Contreras <contreras2004@gmail.com>
2018-11-28 17:20:45 -0300

    Created the project and added pod file

---
+++ Pulent/Pods/Target Support Files/Pods-PulentTests/Pods-PulentTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PulentTests : NSObject
+@end
+@implementation PodsDummy_Pods_PulentTests
+@end
---
+++ Pulent/Pulent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pulent
+//
+//  Created by Matías Contreras on 11/28/18.
+//  Copyright © 2018 Matías Contreras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
